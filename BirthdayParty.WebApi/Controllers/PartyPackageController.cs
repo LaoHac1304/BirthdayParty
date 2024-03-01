b62a70d6c6a27da8b0e2f50b07470355327cdaa3
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-03-01 13:01:57 +0700

    create party package controller

---
+++ BirthdayParty.WebApi/Controllers/PartyPackageController.cs
@@ -0,0 +1,11 @@
+﻿namespace BirthdayParty.WebApi.Controllers;
+
+
+public class PartyPackageController : BaseController<PartyPackageController>
+{
+    public PartyPackageController(ILogger<PartyPackageController> logger) : base(logger)
+    {
+    }
+    
+    
+}
