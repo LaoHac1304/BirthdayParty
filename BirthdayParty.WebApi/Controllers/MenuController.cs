f826d11560249399e259be5afb40f43b5a8e81a8
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-03-01 14:01:08 +0700

    create menu controller

---
+++ BirthdayParty.WebApi/Controllers/MenuController.cs
@@ -0,0 +1,39 @@
+﻿using BirthdayParty.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirthdayParty.WebApi.Controllers;
+
+
+[ApiController]
+[Route("api/v1/menus")]
+public class MenuController : BaseController<MenuController>
+{
+    public MenuController(ILogger<MenuController> logger) : base(logger)
+    {
+    }
+
+    [HttpGet]
+    public Task<ActionResult<IEnumerable<Menu>>> Get()
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpGet("{id}")]
+    public Task<ActionResult<Menu>> Get(string id)
+    {
+        throw new NotImplementedException();
+    }
+    
+   [HttpPut("{id}")] 
+    public Task<ActionResult<Menu>> Put(string id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpDelete("{id}")]
+    public Task<ActionResult<Menu>> Delete(string id)
+    {
+        throw new NotImplementedException();
+    }
+    
+}
