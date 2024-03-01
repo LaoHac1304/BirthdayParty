33f4497235b94a5234ffe425ad3776c25e669555
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-03-01 13:12:43 +0700

    crud endpoints for party packages

--- BirthdayParty.WebApi/Controllers/PartyPackageController.cs
+++ BirthdayParty.WebApi/Controllers/PartyPackageController.cs
@@ -1,10 +1,44 @@
-﻿namespace BirthdayParty.WebApi.Controllers;
+﻿using BirthdayParty.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirthdayParty.WebApi.Controllers;
 
 
+[ApiController]
+[Route("api/v1/party-packages")]
 public class PartyPackageController : BaseController<PartyPackageController>
 {
     public PartyPackageController(ILogger<PartyPackageController> logger) : base(logger)
     {
+    }
+    
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public Task<ActionResult<IEnumerable<PartyPackage>>> Get()
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public Task<ActionResult<PartyPackage>> Post()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public Task<ActionResult<PartyPackage>> Put(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public Task<ActionResult<PartyPackage>> Delete(Guid id)
+    {
+        throw new NotImplementedException();
     }
     
     
