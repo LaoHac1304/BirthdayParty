b4daa5b5916ef295e1ae35747834b12bb3165e4e
Nhan <81398835+LaoHac1304@users.noreply.github.com>
2024-03-17 12:30:43 +0700

    Dev (#30)
    * Features/crud order items (#14)
    
    * commit_crud_orderdetails
    
    * commit crud_orderitems
    
    * commit crud_orderitems
    
    ---------
    
    Co-authored-by: pytas0811 <tekato2002@gmail.com>
    
    * Features/crud party package (#15)
    
    * update gitignore
    
    * create party package controller
    
    * crud endpoints for party packages
    
    * added endpoints for discount, party package and post
    
    * update api endpoint constants
    
    * create menu controller
    
    * update party package controller and post controller
    
    * implement create post
    
    * implemented party packages crud functions
    
    * implemented post crud functions
    
    ---------
    
    Co-authored-by: Gowwre <56794916+Gowwre@users.noreply.github.com>
    
    * config paging and fix conflict
    
    * update gitignore
    
    * hides certain endpoints
    
    * add discount and menu controller
    
    * fix conflict for party_package (#18)
    
    * config naming api
    
    * crud for customer and host (#19)
    
    * config create post request (#21)
    
    * Features/manage post (#27)
    
    * config create post request
    
    * config upload file and manage post
    
    * get user info (#29)
    
    ---------
    
    Co-authored-by: pytas0811 <tekato2002@gmail.com>
    Co-authored-by: Gowwre <56794916+Gowwre@users.noreply.github.com>
    Co-authored-by: sonaincry <tranbaosonga@gmail.com>
    Co-authored-by: sonaincry <84517723+sonaincry@users.noreply.github.com>

--- BirthdayParty.WebApi/Controllers/AuthenticationController.cs
+++ BirthdayParty.WebApi/Controllers/AuthenticationController.cs
@@ -1,9 +1,13 @@
 ﻿using BirthdayParty.Application.Service.Common;
 using BirthdayParty.Domain.Payload.Request.Authentications;
+using BirthdayParty.Domain.Payload.Response.Customers;
+using BirthdayParty.Domain.Payload.Response.HostParties;
 using BirthdayParty.WebApi.Constants;
 using BirthdayParty.WebApi.Middlewares;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BirthdayParty.WebApi.Controllers
 {
@@ -12,10 +16,19 @@ namespace BirthdayParty.WebApi.Controllers
     public class AuthenticationController : BaseController<AuthenticationController>
     {
         private readonly IAuthenticationService _authenticationService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ICustomerService _customerService;
+        private readonly IHostPartyService _hostPartyService;   
         public AuthenticationController(ILogger<AuthenticationController> logger
-            , IAuthenticationService authenticationService) : base(logger)
+            , IAuthenticationService authenticationService,
+              IHttpContextAccessor httpContextAccessor, ICustomerService customerService
+            , IHostPartyService hostPartyService) 
+            : base(logger)
         {
             _authenticationService = authenticationService;
+            _httpContextAccessor = httpContextAccessor;
+            _customerService = customerService;
+            _hostPartyService = hostPartyService;
         }
 
         [HttpPost(ApiEndPointConstant.Authentication.Login)]
@@ -34,6 +47,30 @@ namespace BirthdayParty.WebApi.Controllers
 
             return Ok(loginResponse);
 
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet(ApiEndPointConstant.Authentication.Info)]
+
+        public async Task<IActionResult> GetUserInfo()
+        {
+            var currentUser = _httpContextAccessor.HttpContext.User;
+            if (currentUser is null) return Unauthorized("Email Or Role Invalid!!!");
+
+            var userId = currentUser.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var email = currentUser.FindFirst(ClaimTypes.Email)?.Value;
+            var role = currentUser.FindFirst(ClaimTypes.Role)?.Value;
+            if (role is null || userId is null || email is null) return Unauthorized("Email Or Role Invalid!!!");
+            if (role.Equals("customer"))
+            {
+                GetCustomerResponse customerResponse = await _customerService.GetCustomerByAccountId(userId);
+                return Ok(customerResponse);
+            }
+            if (role.Equals("host"))
+            {
+                GetHostPartyResponse hostResponse = await _hostPartyService.GetHostPartyByAccountId(userId);
+                return Ok(hostResponse);
+            }
+            return Unauthorized("Email Or Role Invalid!!!");
         }
     }
 }
