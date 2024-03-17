83fc7c12233f2a546a3554b68354e8fe5e2df2fe
marchjeff145@gmail.com <marchjeff145@gmail.com>
2024-03-17 15:11:17 +0700

    up controller to upload file

---
+++ BirthdayParty.WebApi/Controllers/FileUploaderController.cs
@@ -0,0 +1,35 @@
+﻿using BirthdayParty.Application.Service;
+using BirthdayParty.Domain.Payload.Request.UploadFile;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirthdayParty.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class FileUploaderController : BaseController<FileUploaderController>
+    {
+        private readonly IUploadFileService _uploadFileService;
+        public FileUploaderController(ILogger<FileUploaderController> logger, IUploadFileService uploadFileService) : base(logger)
+        {
+            _uploadFileService = uploadFileService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm] UploadFileRequest request)
+        {
+            if(request.File == null) { return BadRequest(ModelState); }
+            try
+            {
+                var fileUrl = await _uploadFileService.UploadFile(request.File);
+                return Ok(new
+                {
+                    url = fileUrl,
+                });
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
