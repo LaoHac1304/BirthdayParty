705a361ce5ce28ce3661bb01dbb1395e92592518
Nhan <81398835+LaoHac1304@users.noreply.github.com>
2024-03-20 23:00:50 +0700

    Features/flow package detail (#50)
    * get user info
    
    * show info party package
    
    * config user info
    
    * config order detail
    
    * remove price from menu and fix menu service
    
    * config location for partypackage request
    
    * config searh party package
    
    ---------
    
    Co-authored-by: duybinhngo <duybinhngo2901@gmail.com>

--- BirthdayParty.Domain/Payload/Request/PartyPackages/GetPartyPackagesRequest.cs
+++ BirthdayParty.Domain/Payload/Request/PartyPackages/GetPartyPackagesRequest.cs
@@ -5,6 +5,8 @@ public class GetPartyPackagesRequest
     public string? IsDeleted { get; set; } = "both";
     public string? SearchString { get; set; } = "";
     public string? HostPartyId { get; set; } = "";
+    public string? Location { get; set; } = "";
+    public string? Status { get; set; } = "both";
     public int Page { get; set; }
     public int Size { get; set; }
 }
