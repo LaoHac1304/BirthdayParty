9fe7c870a9d6b7e9a9a9d3dc44a8efe5e49aa11b
duybinhngo <duybinhngo2901@gmail.com>
2024-03-20 22:00:44 +0700

    config location for partypackage request

--- BirthdayParty.Domain/Payload/Request/PartyPackages/GetPartyPackagesRequest.cs
+++ BirthdayParty.Domain/Payload/Request/PartyPackages/GetPartyPackagesRequest.cs
@@ -5,6 +5,7 @@ public class GetPartyPackagesRequest
     public string? IsDeleted { get; set; } = "both";
     public string? SearchString { get; set; } = "";
     public string? HostPartyId { get; set; } = "";
+    public string? Location { get; set; }
     public int Page { get; set; }
     public int Size { get; set; }
 }
