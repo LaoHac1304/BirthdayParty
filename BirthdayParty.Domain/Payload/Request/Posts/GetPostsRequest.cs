388f5896a521412fe90c7e64ecaf38399f5c9a32
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-03-21 11:45:17 +0700

    validate post request models

--- BirthdayParty.Domain/Payload/Request/Posts/GetPostsRequest.cs
+++ BirthdayParty.Domain/Payload/Request/Posts/GetPostsRequest.cs
@@ -4,6 +4,8 @@ namespace BirthdayParty.Domain.Payload.Request.Posts;
 
 public class GetPostsRequest
 {
+    [Range(0, int.MaxValue, ErrorMessage = "Page must be greater than or equal to 0")]
     public int Page { get; set; }
+    [Range(0, int.MaxValue, ErrorMessage = "PageSize must be greater than or equal to 0")]
     public int PageSize { get; set; }
 }
