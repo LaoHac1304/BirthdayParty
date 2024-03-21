934ea8fcb90447878d8b51752680d866ef345c21
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-03-21 11:42:06 +0700

    validate discount request model

--- BirthdayParty.Domain/Payload/Request/Discount/CreateDiscountRequest.cs
+++ BirthdayParty.Domain/Payload/Request/Discount/CreateDiscountRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,11 @@ namespace BirthdayParty.Domain.Payload.Request.Discounts
 {
     public class CreateDiscountRequest
     {
+        [Required(ErrorMessage = "DiscountPercent is required")]
+        [Range(0, 100, ErrorMessage = "DiscountPercent must be between 0 and 100")]
         public long? DiscountPercent { get; set; }
 
+        
         public string? Status { get; set; }
     }
 }
