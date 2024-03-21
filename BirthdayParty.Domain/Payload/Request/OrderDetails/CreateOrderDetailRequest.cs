198a28168a4807ba9bc3f3d9bee1c8c7cd5bbd1d
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-03-21 11:44:30 +0700

    validate order details request models

--- BirthdayParty.Domain/Payload/Request/OrderDetails/CreateOrderDetailRequest.cs
+++ BirthdayParty.Domain/Payload/Request/OrderDetails/CreateOrderDetailRequest.cs
@@ -13,8 +13,11 @@ namespace BirthdayParty.Domain.Payload.Request.OrderDetails
 
         public string? ChildrenName { get; set; }
         public DateTime? ChildrenBirthday { get; set; }
+        [Range(0,10,ErrorMessage = "Number of children must be between 0 and 10")]
         public int? NumberOfChildren { get; set; }
+        
         public string? Gender { get; set; }
+        [Range(0, 1000, ErrorMessage = "Total price must be between 0 and 1000")]
         public long? TotalPrice { get; set; }
         public DateTime? StartTime { get; set; }
 
