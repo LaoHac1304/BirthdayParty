fed2685131cb7629783680849cb664fe917e7969
marchjeff145@gmail.com <marchjeff145@gmail.com>
2024-03-21 23:36:44 +0700

    update room on duty request query

--- BirthdayParty.Domain/Payload/Request/RoomOnDutys/GetRoomOnDutysRequest.cs
+++ BirthdayParty.Domain/Payload/Request/RoomOnDutys/GetRoomOnDutysRequest.cs
@@ -2,11 +2,11 @@
 
 namespace BirthdayParty.Domain.Payload.Request.RoomOnDutys;
 
-public class GetMenuRequest
+public class GetRoomOnDutysRequest
 {
-    public string? Status { get; set; } = "both";
+    public string? Status { get; set; } = "";
     public string? SearchString { get; set; } = "";
-    public string? HostPartyId { get; set; } = "";
+    //public string? HostPartyId { get; set; } = "";
     [Range(0, int.MaxValue, ErrorMessage = "Page must be greater than or equal to 0")]
     public int Page { get; set; }
     [Range(0, int.MaxValue, ErrorMessage = "Size must be greater than or equal to 0")]
