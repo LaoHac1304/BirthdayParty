b1b46636469bd751f39f0745546ea60fc00e6fb4
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-03-21 16:33:15 +0700

    validate menu request models

--- BirthdayParty.Domain/Payload/Request/Menu/CreateMenuRequest.cs
+++ BirthdayParty.Domain/Payload/Request/Menu/CreateMenuRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace BirthdayParty.Domain.Payload.Request.Menu
 
         public string? Description { get; set; }
 
+        [Range(0,long.MaxValue,ErrorMessage = "Price must be greater than 0")]
         public long? Price { get; set; }
 
         public string? PartyPackageId { get; set; }
