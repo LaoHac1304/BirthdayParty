890c4578c01fe62b8eada330e42e473dcbbab8b0
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-03-21 11:45:32 +0700

    validate room on duties request models

--- BirthdayParty.Domain/Payload/Request/RoomOnDutys/CreateRoomOnDutyRequest.cs
+++ BirthdayParty.Domain/Payload/Request/RoomOnDutys/CreateRoomOnDutyRequest.cs
@@ -1,9 +1,12 @@
-﻿namespace BirthdayParty.Domain.Payload.Request.RoomOnDutys;
+﻿using BirthdayParty.Domain.Validation.RoomOnDutyValidation;
+
+namespace BirthdayParty.Domain.Payload.Request.RoomOnDutys;
 
 public class CreateRoomOnDutyRequest
 {
     public string PartyPackageId { get; set; }
     public DateTime StartDate { get; set; }
+    [RoomOnDutyDate(startDate: nameof(StartDate), errorMessage: "End Date must be after Start Date")]
     public DateTime EndDate { get; set; }
     public string? Status { get; set; }
 }
