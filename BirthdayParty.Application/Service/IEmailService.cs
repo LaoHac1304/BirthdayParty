c393d5ef2c81f51b8ededd1cc74c645dae50fe7e
duybinhngo <duybinhngo2901@gmail.com>
2024-03-21 20:14:58 +0700

    config send email to confirm Order

---
+++ BirthdayParty.Application/Service/IEmailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayParty.Application.Service
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string orderDetailId);
+    }
+}
---
+++ BirthdayParty.Domain/Payload/Request/OrderDetails/GmailOrderDetailRequest.cs
@@ -0,0 +1,22 @@
+﻿using BirthdayParty.Domain.Validation.OrderDetailValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayParty.Domain.Payload.Request.OrderDetails
+{
+    public class GmailOrderDetailRequest
+    {
+        public string? ChildrenName { get; set; }
+        public long? TotalPrice { get; set; }
+        public DateTime? StartTime { get; set;}
+        public DateTime? EndTime { get; set;}
+        public DateTime? ChildrenBirthday { get; set; }
+        public int? NumberOfChildren { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public string? PackageName { get; set; }
+        public string? HostPartyPhoneNumber { get; set; }
+    }
+}
